6d6410c9654605b595bb2375de8727346fe3ab4c
ocecer <54540749+ocecer@users.noreply.github.com>
2022-06-21 21:42:20 +0200

    Create iplookup.py

---
+++ iplookup.py
@@ -0,0 +1,29 @@
+# import urllib.request
+# import json
+
+# with urllib.request.urlopen("https://geolocation-db.com/json") as url:
+#     data = json.loads(url.read().decode())
+#     print(data)
+
+
+import requests
+
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return location_data
+
+
+print(get_location())
