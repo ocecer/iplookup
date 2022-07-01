e9fdf0789a6808b09ccb80e93dc116be3b8abc84
ocecer <54540749+ocecer@users.noreply.github.com>
2022-07-01 08:40:46 +0200

    Update iplookup.py

--- iplookup.py
+++ iplookup.py
@@ -1,33 +1,3 @@
-# import urllib.request
-# import json
-
-# with urllib.request.urlopen("https://geolocation-db.com/json") as url:
-#     data = json.loads(url.read().decode())
-#     print(data)
-
-
-# import requests
-
-
-# def get_ip():
-#     response = requests.get('https://api64.ipify.org?format=json').json()
-#     return response["ip"]
-
-
-# def get_location():
-#     ip_address = get_ip()
-#     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-#     location_data = {
-#         "ip": ip_address,
-#         "city": response.get("city"),
-#         "region": response.get("region"),
-#         "country": response.get("country_name")
-#     }
-#     return location_data
-
-
-# print(get_location())
-
 import ipapi
 from flask import Flask, request, render_template
 
@@ -43,4 +13,5 @@ def Index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(port=5000)
